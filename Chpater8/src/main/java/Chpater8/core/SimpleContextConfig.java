3d3903da45719e1d817aa3b72f17c08355f0a75e
Vendredimatin <2860957268@qq.com>
2019-06-04 17:12:16 +0800

    commit chapter 8

---
+++ Chpater8/src/main/java/Chpater8/core/SimpleContextConfig.java
@@ -0,0 +1,23 @@
+package Chpater8.core;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.Lifecycle;
+import org.apache.catalina.LifecycleEvent;
+import org.apache.catalina.LifecycleListener;
+
+/**
+ * @program: SimpleTomcat
+ * @description: StandardContext的监听器类
+ * @author: Liu Hanyi
+ * @create: 2019-06-04 16:50
+ **/
+
+public class SimpleContextConfig implements LifecycleListener {
+
+    public void lifecycleEvent(LifecycleEvent event) {
+        if (Lifecycle.START_EVENT.equals(event.getType())) {
+            Context context = (Context) event.getLifecycle();
+            context.setConfigured(true);
+        }
+    }
+}
